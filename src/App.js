dc26084213f8e5419672c55ddb2934a3e9b6c141
Gaunk <ifdarus@yahoo.com>
2021-03-02 03:46:21 -0800

    Bobo Dulu ahk ngantuk

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import NavbarFunc from './Component/Func/NavbarFunc';
+import HomeFunc from './Component/Func/HomeFunc';
+import AboutFunc from './Component/Func/AboutFunc';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavbarFunc/>
+      <Switch>
+        <Route exact path='/' component={HomeFunc} />
+        <Route exact path='/about' component={AboutFunc} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
